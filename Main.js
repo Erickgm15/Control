a9b9f6b135d12bb380247abcfa2084ad1bd2305f
Erick <erickgarcia200811@gmail.com>
2025-07-22 09:21:32 -0600

    Titulo y parrafo js

---
+++ Main.js
@@ -0,0 +1,22 @@
+ function seccion (){
+
+   let seccion = document.createElement('section');
+
+   
+   let h1 = document.createElement('h1')
+    h1.className = "titulo";
+   h1.innerText = "HOLA MUNDO ";
+   seccion.appendChild(h1);
+
+
+   let p = document.createElement('p')
+   p.className = "parrafo";
+   p.innerText = " Introduccion a JavaScript";
+   seccion.appendChild(p);
+
+return seccion;
+
+}
+
+document.body.appendChild(seccion());
+
